fe366075fa6938b3ac0cd3c222479d5889cc2142
misha <misha.skubenich@gmail.com>
2014-04-29 13:08:51 +0300

    include phantom.js

--- crawler.rb
+++ crawler.rb
@@ -1,5 +1,17 @@
+require 'rubygems'
+require 'capybara'
+require 'capybara-webkit'
+require 'capybara-screenshot'
+include Capybara::DSL
+
 namespace :crawler do
   task :grab do
-    puts "hello!!!"
+    Capybara.current_driver = :webkit
+    Capybara.app_host = 'http://auto.inforico.com.ua'
+    page.visit('/')
+    puts '-------------------'
+    puts page.text
+    puts '-------------------'
+    page.screenshot_and_open_image
   end
 end
