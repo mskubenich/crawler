21b84e7acece16cae72aff3fec9587c7d377d470
misha <misha.skubenich@gmail.com>
2014-05-01 11:34:45 +0300

    parse sitemap

--- crawler.rb
+++ crawler.rb
@@ -7,11 +7,60 @@ include Capybara::DSL
 namespace :crawler do
   task :grab do
     Capybara.current_driver = :webkit
-    Capybara.app_host = 'http://auto.inforico.com.ua'
+    Capybara.app_host = 'http://inforico.com.ua'
     page.visit('/')
-    puts '-------------------'
-    puts page.text
-    puts '-------------------'
-    page.screenshot_and_open_image
+
+    categories = Hash[page.all(:css, '.index-cat-title > span > a').map{|cat| [cat.text, cat[:href]]}]
+
+    puts 'Get categories...'
+    categories.each do |cat, cat_link|
+      puts "#{ cat }:   #{cat_link}"
+    end
+
+    puts ""
+    puts ""
+    categories.each do |title, link|
+      puts "Go to #{ title }..."
+      puts "Get subcategories..."
+      page.visit link
+
+      sub_categories = Hash[page.all(:css, '.sf-subcats-a').map{|cat| [cat.text, cat[:href]]}]
+      sub_categories.each do |cat, cat_link|
+        puts "Go to subcategory #{ cat }:   #{cat_link}..."
+
+        page.visit cat_link
+
+        puts "Get operations..."
+        operations = Hash[page.all(:css, '.sf-type').map{|cat| [cat.text, cat[:href]]}]
+
+        operations.each do |operation, oper_link|
+          puts "#{ operation }:   #{oper_link}"
+
+
+          puts "Go to operation..."
+          page.visit oper_link
+
+          puts "Get items..."
+          items = Hash[page.all(:css, '.item:not(.item-top) .alvi-title a').map{|cat| [cat.text, cat[:href]]}]
+
+          items.each do |item, item_link|
+            puts "#{ item }:   #{item_link}"
+          end
+
+          break #TODO remove this
+        end
+
+
+        break #TODO remove this
+      end
+
+      puts ""
+      puts ""
+
+      break #TODO remove this
+      #page.screenshot_and_open_image
+    end
+
+
   end
 end
